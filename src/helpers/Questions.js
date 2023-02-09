bc456b1d62f0e61413747b74d38a6f55edf2d25c
ravimishra2810 <106543997+ravimishra2810@users.noreply.github.com>
2023-02-09 13:07:10 +0530

    Initial commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,53 @@
+export const Questions = [
+    {
+      prompt: "What is metal au called?",
+      optionA: "Gold",
+      optionB: "Silver",
+      optionC: "Tin",
+      optionD: "Zinc",
+      asnwer: "optionA",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "MC-03",
+      optionD: "Java",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+
+
+    {
+        prompt: "What is the first step to upload your react project on github?",
+        optionA: "Open Github",
+        optionB: "Upload only SRC folder",
+        optionC: "Delete Node modules",
+        optionD: "Delete Index.html file",
+        asnwer: "optionC",
+      },
+      {
+        prompt: "Which of this is a programming language?",
+        optionA: "Reduce",
+        optionB: "JavaScript",
+        optionC: "MC-03",
+        optionD: "Mac book",
+        asnwer: "optionB",
+      },
+      {
+        prompt: "Which of this is not planet?",
+        optionA: "Venus",
+        optionB: "Lux",
+        optionC: "Mercury",
+        optionD: "Dharti",
+        asnwer: "optionB",
+      },
+  ];
+  
